0e6900531e7a377d652259fc2bbbd1cad9a48b3c
rashiinfotech <151115705+rashiinfotech@users.noreply.github.com>
2024-07-31 20:07:40 +0530

    cart mgmt compleat

--- middelware/passport.js
+++ middelware/passport.js
@@ -27,7 +27,8 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://rashi.i.store.solutions/auth/google/callback"
+        // callbackURL: "http://localhost:8000/auth/google/callback"
+        callbackURL: "https://rashi.istore.solutions/auth/google/callback"
     },
     async function(token, tokenSecret, profile, done) {
         try {
